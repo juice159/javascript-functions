84db63142d87f824440a4edca7fa129a09bd4cf5
Thanutham Baikwang <thanutham.j@gmail.com>
2024-08-26 16:35:35 +0700

    fix: fixed answer on ex-5

--- ex-5.js
+++ ex-5.js
@@ -1,10 +1,6 @@
 // Start coding here
-const minNumber = findMinNumber(10, 20);
-
-console.log(minNumber); // 10
-
 function findMinNumber(a, b){
     return Math.min(a, b);
-};
-let result = findMinNumber(120, 500);
-console.log(result);
+  }
+  const minNumber = findMinNumber(10, 20);
+  console.log(minNumber); // 10
